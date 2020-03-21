9a084a13841686958753414e80e7ea4e18733674
Ryota <freehazamanet@gmail.com>
2020-03-21 06:48:37 +0000

    Create Basic Login

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'bcrypt',         '3.1.12'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 gem 'bootstrap-sass', '3.3.7'
-
+gem 'jquery-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/signup', to: 'users#new'
-  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
 end
