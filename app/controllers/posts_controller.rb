87b59e6562e96e87402f87cf3366238a3612345b
Ryota <freehazamanet@gmail.com>
2020-03-27 00:13:49 +0000

    Create Post Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
+  
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿されました"
+      redirect_to users_path
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    flash[:success] = "あたなの投稿は削除されました"
+    redirect_to request.referrer || root_url
+  end
+  
+  private
+    
+    def post_params
+      params.require(:post).permit(:content)
+    end
+    
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 end
