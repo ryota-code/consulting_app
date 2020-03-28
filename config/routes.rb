6afd0a168e7e10c600dc718bad631deebc3b231d
Ryota <freehazamanet@gmail.com>
2020-03-28 08:55:21 +0000

    Show Following Status

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts, only: [:create, :destroy]
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :posts, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
